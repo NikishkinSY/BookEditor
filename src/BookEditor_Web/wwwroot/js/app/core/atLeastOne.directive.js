a16ac7c42eff93f0556e23e49101ff2499aeb96e
Sergey Nikishkin <sergey_nikishkin@adoriasoft.com>
2016-10-09 23:41:35 +0300

    - create fundament of directive

--- src/BookEditor_Web/wwwroot/js/app/core/atLeastOne.directive.js
+++ src/BookEditor_Web/wwwroot/js/app/core/atLeastOne.directive.js
@@ -10,7 +10,21 @@
     function ngAtLeastOne() {
         return {
             link: function (scope, element, attr) {
-                
+                var test = 0;
+                //var blacklist = attr.blacklist.split(',');
+
+                ////For DOM -> model validation
+                //ngModel.$parsers.unshift(function (value) {
+                //    var valid = blacklist.indexOf(value) === -1;
+                //    ngModel.$setValidity('blacklist', valid);
+                //    return valid ? value : undefined;
+                //});
+
+                ////For model -> DOM validation
+                //ngModel.$formatters.unshift(function (value) {
+                //    ngModel.$setValidity('blacklist', blacklist.indexOf(value) === -1);
+                //    return value;
+                //});
             }
         };
     }
